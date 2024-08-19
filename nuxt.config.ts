99827cc3aec55c5af361dcbbb25cb5985143b1b1
NovaliX <novalix@protonmail.com>
2024-08-19 16:10:39 +0200

    added the config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,66 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  ssr: false,
+  devtools: { enabled: true },
+
+  modules: [
+    '@pinia/nuxt',
+    'shadcn-nuxt',
+    '@nuxtjs/tailwindcss',
+    'nuxt-monaco-editor',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-lucide-icons',
+    '@nuxt/image',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/google-fonts',
+  ],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+
+  content: {
+    highlight: {
+      theme: {
+        // Default theme (same as single string)
+        default: 'github-light',
+        dark: 'github-dark',
+      },
+      langs: ['c', 'cpp', 'java', 'python'],
+    },
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  googleFonts: {
+    families: {
+      Inter: '300..800',
+      'JetBrains+Mono': '200..800',
+    },
+    display: 'swap',
+  },
+
+  monacoEditor: {
+    locale: 'fr',
+  },
+
+  compatibilityDate: '2024-07-04',
+
+  nitro: {
+    preset: 'bun',
+    compressPublicAssets: {
+      brotli: true
+    }
+  },
+});
